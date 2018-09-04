9a0df164d737e220188c83de1e7bb41e01811830
张威 <294341493@qq.com>
2018-09-04 17:14:49 +0800

    8080端口号被占用后使用空闲端口

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,14 +1,17 @@
 const merge = require('webpack-merge')
+const portfinder = require('portfinder')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const baseConf = require('./webpack.base.conf')
 const { port } = require('../config')
 
-module.exports = merge(baseConf, {
+const devWebpackConfig = merge(baseConf, {
   mode: 'development',
   devServer: {
     compress: true,
+    https: false,
     port,
-    open: true
+    open: false,
+    proxy: {}
   },
   module: {
     rules: [
@@ -27,4 +30,16 @@ module.exports = merge(baseConf, {
       template: 'index.html'
     })
   ]
+})
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = port
+  portfinder.getPort((err, newPort) => {
+    if (err) {
+      reject(err)
+    } else {
+      devWebpackConfig.devServer.port = newPort
+      resolve(devWebpackConfig)
+    }
+  })
 })
